e1cc5f213609dd2d0d21e8900f353fe6c40d76c6
amyhuimin <amylimhuimin@gmail.com>
2023-01-07 02:09:33 +0900

    Revert "Post seed data error fix"
    This reverts commit 83419fdb770e3ad2adb1ffc564a2affdf053a6bf.

--- seeders/20221210130676-seed-PostsData.js
+++ seeders/20221210130676-seed-PostsData.js
@@ -35,7 +35,7 @@ module.exports = {
         {
           Date: "01 Jun 2021",
           UserId: 4,
-          IdeaId: 1,
+          IdeaId: 2,
           User: "Beyonce",
           IdeaName: "Toy with human interaction",
           OneLiner: "Making your favourite toy a true companion",
